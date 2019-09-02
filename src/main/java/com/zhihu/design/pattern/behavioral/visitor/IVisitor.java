e681d45e6f16edb62022592db40b70968d729aa2
guozhihao <283839751@qq.com>
2019-09-02 23:10:13 +0800

    访问者模式案例

--- src/main/java/com/zhihu/design/pattern/behavioral/visitor/IVisitor.java
+++ src/main/java/com/zhihu/design/pattern/behavioral/visitor/IVisitor.java
@@ -6,7 +6,7 @@ package com.zhihu.design.pattern.behavioral.visitor;
  * Date: Create in 2019/9/1 23:25
  */
 public interface IVisitor {
-
+    
     void visit(FreeCourse freeCourse);
     
     void visit(CodingCourse codingCourse);
